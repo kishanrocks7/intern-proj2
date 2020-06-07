d5307966f73e53fb63b0e1c11b847323e53f9aad
umeshkv2 <umesh.us.suman@gmail.com>
2020-06-07 17:11:55 +0530

    directory structure changed

--- js/faq.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-$('.content').on('click', function(){
-  $(this).toggleClass('opened');
-});
- 
-
-});
-
--- main.py
+++ main.py
@@ -11,7 +11,21 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/outletlogin')
+def warehouselogin():
+    return render_template('outletlogin.html')
 
+@app.route('/warehouselogin')
+def outletlogin():
+    return render_template('warehouselogin.html')
+
+@app.route('/blogs')
+def blogs():
+    return render_template('blog.html')
+
+@app.route('/faqs')
+def faqs():
+    return render_template('faq.html')
 
     # Run from here
 if __name__ == "__main__":
