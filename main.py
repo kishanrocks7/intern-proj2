6450c74ca93eaf4a294ced38ca5426ebd012d709
umeshkv2 <umesh.us.suman@gmail.com>
2020-06-10 01:56:47 +0530

    changes

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 #importing neccessary libraries
-from flask import Flask,render_template,request,session,redirect,url_for,flash
+from flask import Flask,render_template,request,session,redirect,url_for,flash   
+
 #importing database library
-import uuid
-from warehouse import wreg
 from databaselibrary import getdbcur
 
+#warehouse functions
+from warehouse import wdashboard
+
+#universal unique ID package
+import uuid,pybase64
+
+from flask_mail import Mail, Message
 # This is main app point
 app = Flask(__name__)
+
+
+# mail configs
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'codewithash99@gmail.com'
+app.config['MAIL_PASSWORD'] = '012345aB'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+
+mail = Mail(app)
+
 app.secret_key= 'secret4key'
 #ROUTES
 
@@ -15,9 +35,13 @@ app.secret_key= 'secret4key'
 def home():
     return render_template('index.html')
 
-@app.route('/warehouselogin')
-def warehouselogin():
-    return render_template('warehouselogin.html')
+@app.route('/warehouse_login')
+def warehouse_login():
+    return  render_template('warehouselogin.html')
+
+@app.route('/warehouse_dashboard', methods = ['GET','POST'] )
+def warehouse_dashboard():
+    return wdashboard()
 
 @app.route('/outlet_login')
 def outlet_login():
@@ -29,11 +53,40 @@ def outlet_register():
 
 @app.route('/warehouse_register',methods = ['GET','POST'])
 def warehouse_register():
-    return wreg()
+    if request.method =='POST':
+        fullkey = uuid.uuid4()
+        uid = fullkey.time
+        em = request.form['email']
+        wname = str(pybase64.b64encode((request.form['warehouseName']).encode("utf-8")),"utf-8")
+        mname = str(pybase64.b64encode((request.form['managerName']).encode("utf-8")),"utf-8")
+        email = str(pybase64.b64encode((request.form['email']).encode("utf-8")),"utf-8")
+        compno = str(pybase64.b64encode((request.form['companyNumber']).encode("utf-8")),"utf-8")
+        pno = str(pybase64.b64encode((request.form['phoneNumber']).encode("utf-8")),"utf-8")
+        address = str(pybase64.b64encode((request.form['address']).encode("utf-8")),"utf-8")
+        password  = str(pybase64.b64encode((request.form['password']).encode("utf-8")),"utf-8")
+        cur = getdbcur()
+        checkusersql = "select * from warehouse where (email = %s OR phoneNumber = %s )"
+        cur.execute(checkusersql,(email,pno))
+        checkusercount = cur.rowcount
+        if checkusercount == 0 :
+            insertusersql = 'insert into warehouse(id,warehouseName,managerName,companyNumber,phoneNumber,email,address,password) values (%s,%s,%s,%s,%s,%s,%s,%s) '
+            cur.execute(insertusersql,(uid,wname,mname,compno,pno,email,address,password))
+            insertcount = cur.rowcount
+            if insertcount >= 1 :
+                subject = "Your registration successful !"
+                msg = Message(subject, sender = 'codewithash99@gmail.com', recipients = [str(em)])
+                msg.body = "Hey ! you are registered with us. \n Your unique id to access the dashboard is" + str(uid) + "\n Save this key for your future reference !!"
+                mail.send(msg) 
+                flash('You have succesfully register, You can now login!')
+                return redirect(url_for('warehouse_login'))
+            else:
+                return render_template('warehouseregister.html',failuremsg = "There is error!")
+        else:
+            return render_template('warehouseregister.html',failuremsg = "User is already registered with same Email or Phone Number!")
+    return render_template('warehouseregister.html')
 
-@app.route('/warehouse_login')
-def warehouse_login():
-    return render_template('warehouselogin.html')
+
+
 
 @app.route('/forgot_password')
 def forgot_password():
@@ -83,9 +136,7 @@ def database_crud():
 def staff_crud():
     return render_template('staffcrud.html')
 
-@app.route('/warehouse_dashboard')
-def warehouse_dashboard():
-    return render_template('warehouse_dashboard.html')
+
 
 @app.route('/outlet_dashboard')
 def outlet_dashboard():
--- warehouse.py
+++ warehouse.py
@@ -1,20 +1,20 @@
-import uuid,base64
+import uuid,pybase64
 from databaselibrary import getdbcur
 from flask import Flask,render_template,request,session,redirect,url_for,flash
-
-password_key = 'secret4password'
+from flask_mail import Mail, Message
 
 def wreg():
     if request.method =='POST':
         fullkey = uuid.uuid4()
         uid = fullkey.time
-        wname = str(base64.b64encode((request.form['warehouseName']).encode("utf-8")),"utf-8")
-        mname = str(base64.b64encode((request.form['managerName']).encode("utf-8")),"utf-8")
-        email = str(base64.b64encode((request.form['email']).encode("utf-8")),"utf-8")
-        compno = str(base64.b64encode((request.form['companyNumber']).encode("utf-8")),"utf-8")
-        pno = str(base64.b64encode((request.form['phoneNumber']).encode("utf-8")),"utf-8")
-        address = str(base64.b64encode((request.form['address']).encode("utf-8")),"utf-8")
-        password  = str(base64.b64encode((request.form['password']).encode("utf-8")),"utf-8")
+        em = request.form['email']
+        wname = str(pybase64.b64encode((request.form['warehouseName']).encode("utf-8")),"utf-8")
+        mname = str(pybase64.b64encode((request.form['managerName']).encode("utf-8")),"utf-8")
+        email = str(pybase64.b64encode((request.form['email']).encode("utf-8")),"utf-8")
+        compno = str(pybase64.b64encode((request.form['companyNumber']).encode("utf-8")),"utf-8")
+        pno = str(pybase64.b64encode((request.form['phoneNumber']).encode("utf-8")),"utf-8")
+        address = str(pybase64.b64encode((request.form['address']).encode("utf-8")),"utf-8")
+        password  = str(pybase64.b64encode((request.form['password']).encode("utf-8")),"utf-8")
         cur = getdbcur()
         checkusersql = "select * from warehouse where (email = %s OR phoneNumber = %s )"
         cur.execute(checkusersql,(email,pno))
@@ -24,10 +24,38 @@ def wreg():
             cur.execute(insertusersql,(uid,wname,mname,compno,pno,email,address,password))
             insertcount = cur.rowcount
             if insertcount >= 1 :
+                subject = "Your registration successful !"
+                msg = Message(subject, sender = 'codewithash99@gmail.com', recipients = [str(em)])
+                msg.body = "Hey ! you are registered with us. \n Your unique id to access the dashboard is" + str(uid) + "\n Save this key for your future reference !!"
+                mail.send(msg) 
                 flash('You have succesfully register, You can now login!')
-                return redirect(url_for('warehouselogin'))
+                return redirect(url_for('warehouse_login'))
             else:
                 return render_template('warehouseregister.html',failuremsg = "There is error!")
         else:
             return render_template('warehouseregister.html',failuremsg = "User is already registered with same Email or Phone Number!")
     return render_template('warehouseregister.html')
+
+
+def wdashboard():
+    if request.method == "POST":
+        user_id = request.form['userid']
+        em_or_num = str(pybase64.b64encode((request.form['em_or_num']).encode("utf-8")),"utf-8")
+        manager_name = request.form['manager_name']
+        mname = str(pybase64.b64encode((request.form['manager_name']).encode("utf-8")),"utf-8")
+        passwd = str(pybase64.b64encode((request.form['password']).encode("utf-8")),"utf-8")
+        cur = getdbcur()
+        sql = "select managerName from warehouse where (id = %s  AND (email = %s OR phoneNumber = %s ) AND managerName=%s AND password= %s ) "
+        cur.execute(sql,(user_id, em_or_num, em_or_num , mname, passwd))
+        sqlres = cur.rowcount
+        mname = cur.fetchone
+        print(mname)
+        if sqlres == 1 :
+            session['user_id'] = user_id 
+            return render_template('warehouse_dashboard.html', m_name = manager_name)
+        else:
+            flash("Incorrect credentials!")
+            return redirect(url_for('warehouse_login'))
+    return redirect(url_for('warehouse_login'))
+
+    
